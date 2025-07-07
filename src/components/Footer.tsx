a50a1b22eba03de3536b8ffa1563ad0e44623d54
NK95 <nagham.qarqamaz.kp@gmail.com>
2025-07-07 17:23:52 +0300

    .

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,21 +5,23 @@ export default function Footer() {
     return (
         <footer>
             <img className="w-full h-40" src={loadImage("images/wave.png")} />
-            <div className="bg-pumpkin text-white pb-8 px-4 max-w-6xl mx-auto space-y-6 text-sm">
-                <ul className="flex justify-center flex-wrap gap-x-4 gap-y-2">
-                    {navItems.map((item, index) => (
-                        <NavItem
-                            key={index}
-                            href={item.href}
-                            text={item.text}
-                            whiteLine
-                        />
-                    ))}
-                </ul>
-                <p className="text-center">
-                    © {new Date().getFullYear()} Nagham Qarqamaz. All rights
-                    reserved.
-                </p>
+            <div className="bg-pumpkin text-white pb-8 px-4 space-y-6 text-sm">
+                <div className="max-w-6xl mx-auto">
+                    <ul className="flex justify-center flex-wrap gap-x-4 gap-y-2">
+                        {navItems.map((item, index) => (
+                            <NavItem
+                                key={index}
+                                href={item.href}
+                                text={item.text}
+                                whiteLine
+                            />
+                        ))}
+                    </ul>
+                    <p className="text-center">
+                        © {new Date().getFullYear()} Nagham Qarqamaz. All rights
+                        reserved.
+                    </p>
+                </div>
             </div>
         </footer>
     );
